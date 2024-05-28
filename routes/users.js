38c5be97fa172d054e150418f6007d7acceb369f
최한얼 <choehan-eol@choehan-eol-ui-MacBookPro.local>
2024-05-28 10:33:29 +0900

    modify insert exercise-record

--- routes/users.js
+++ routes/users.js
@@ -14,8 +14,26 @@ router.post('/', function (req, res, next) {
             res.send("duplicate fail")
         } else {
             // 회원가입 시 유저 운동 세트 및 목표치 설정 알고리즘 들어가는 부분(현재는 스쿼트로 고정)
+            var birthDate = new Date(req.body.birth.substr(0, 4),
+                req.body.birth.substr(4,2) -1,
+                req.body.birth.substr(6,2));
+            var currentDate = new Date();
+            var age = currentDate.getFullYear() - birthDate.getFullYear()
+            if (currentDate < new Date(currentDate.getFullYear(), birthDate.getMonth(), birthDate.getDate())) {
+                age--;
+            }
+
+            const baseSquatCount = 30;
+            const ageAdjustment = Math.floor(age /5);
+            const weightAdjustment = Math.floor(req.body.weight / 10);
+            const heightAdjustment = Math.floor(req.body.height / 100);
+            let recommendedCount = baseSquatCount - ageAdjustment - weightAdjustment - heightAdjustment;
+            if (recommendedCount < 5) {
+                recommendedCount = 5;
+            }
+
             var sql = "insert into user_exercise values (?,?,?,?)"
-            var params = [null, req.body.id, "squarts", 20]
+            var params = [null, req.body.id, "squarts", recommendedCount]
             console.log(sql)
             console.log(params)
             conn.query(sql, params, function (err, result) {
@@ -60,16 +78,45 @@ router.post('/login', function (req, res, next) {
     })
 })
 
-// 유저별 운동세트 가져오
+// 유저별 운동세트 가져오기
 router.get('/:id/exercise-set', function (req, res, next) {
-    console.log(req.params.id)
+    var sql = "select * from user_exercise where user_id = ?";
+    var params = [req.params.id]
+    conn.query(sql, params, function (err, result){
+        if (err) {
+            console.log("err err", err)
+        } else {
+            // 마지막 유저의 운동 횟수로 다음 운동 목표 횟수 설정 알고리즘
+            console.log(result)
+            var sql = "select id, do_count from user_exercise_record where user_exercise_id = ? order by id desc limit 1"
+            var params = [result[0].user_exercise_id]
+            conn.query(sql, params, function (err, result){
+                if (err) {
+                    console.log("err err", err)
+                } else {
+                    // 마지막 유저의 운동 횟수로 다음 운동 목표 횟수 설정 알고리즘
+                    console.log(result)
+                    if (result.length !== 0) {
+
+                    }
+                }
+            })
+        }
+    })
+
+
+
     var sql = "select * from user_exercise where user_id = ?";
     var params = [req.params.id]
     conn.query(sql, params, function (err, user_info) {
         if (err) {
             console.log("query is not excuted: " + err);
             res.send("fail")
-        } else res.send(user_info)
+        } else {
+
+
+            res.send(user_info)
+        }
     })
 });
 
@@ -89,8 +136,12 @@ router.get('/exercise-record/:id', function (req, res, next) {
 // 유저 운동 기록 입력하기
 router.post('/exercise-record', function (req, res, next) {
     console.log(req.body)
-    var sql = "insert into user_exercise_record values (?,?, ?, ?)";
-    var params = [null, req.body.user_exercise_id, req.body.target_count, req.body.do_count]
+    var workDate = new Date(req.body.work_date.substr(0, 4),
+        req.body.work_date.substr(4,2) -1,
+        req.body.work_date.substr(6,2));
+
+    var sql = "insert into user_exercise_record values (?,?, ?, ?, ?)";
+    var params = [null, req.body.user_exercise_id, req.body.target_count, req.body.do_count, workDate]
     conn.query(sql, params, function (err, user_info) {
         if (err) {
             console.log("query is not excuted: " + err);
